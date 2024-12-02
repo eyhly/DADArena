98637e60949e4a551391ba421e52071c1b377340
helya <112296143+eyhly@users.noreply.github.com>
2024-12-02 17:35:22 +0700

    add cssbaseline

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./routes";
-import { ThemeProvider } from "@mui/material";
+import { CssBaseLine, ThemeProvider } from "@mui/material";
 import ColorTheme from "./utils/ColorTheme";
 
 const App = () =>  {
 
   return (
     <ThemeProvider theme={ColorTheme}>
+      <CssBaseLine/>
       <RouterProvider router={router}  />
     </ThemeProvider>
   );
