0b0d48d463495c80cfb20c97ca14c73d8c20b6c4
helya <helyasabilla@gmail.com>
2024-12-02 18:05:33 +0700

    fix mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { RouterProvider } from "react-router-dom";
 import router from "./routes";
-import { ThemeProvider } from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import ColorTheme from "./utils/ColorTheme";
 
 const App = () =>  {
 
   return (
     <ThemeProvider theme={ColorTheme}>
+      <CssBaseline/>
       <RouterProvider router={router}  />
     </ThemeProvider>
   );
